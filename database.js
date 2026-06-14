28e10456bd843384b4d215e9334f94a7d7dfd104
Shriparna <shriparna2006@gmail.com>
2026-06-14 23:45:51 +0530

    feat: support persistent disk environment variable in SQLite dbPath

--- database.js
+++ database.js
@@ -4,7 +4,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const dbPath = path.join(__dirname, 'data.db');
+const dbDir = process.env.PERSISTENT_DIR || __dirname;
+const dbPath = path.join(dbDir, 'data.db');
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
